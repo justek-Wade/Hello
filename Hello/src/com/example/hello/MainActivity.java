85419e8e49a21e02eddd1d5c57f9daa81ff6432c
Wade <wade@justek.us>
2013-11-26 23:05:52 +0800

    wade

--- Hello/src/com/example/hello/MainActivity.java
+++ Hello/src/com/example/hello/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onResume() {
     	// TODO Auto-generated method stub
-    	//
+    	//wade
     	//
     	System.out.println("wade");
     	super.onResume();
