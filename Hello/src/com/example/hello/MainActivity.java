8797c7249075d47f1df104040e0b7f5fb5ce6f0c
Wade <wade@justek.us>
2013-11-26 10:19:30 +0800

    wade

--- Hello/src/com/example/hello/MainActivity.java
+++ Hello/src/com/example/hello/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.hello;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -24,6 +25,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onResume() {
     	// TODO Auto-generated method stub
+    	System.out.println("wade");
     	super.onResume();
     }
     
